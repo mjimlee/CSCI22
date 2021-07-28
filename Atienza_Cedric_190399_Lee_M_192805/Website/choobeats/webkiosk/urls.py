88c7ea2ec6216f4dea57fce23c978cda953fb588
M Jim Lee <mjimlee@gmail.com>
2021-07-29 00:33:45 +0800

    customer django changes

--- Atienza_Cedric_190399_Lee_M_192805/Website/choobeats/webkiosk/urls.py
+++ Atienza_Cedric_190399_Lee_M_192805/Website/choobeats/webkiosk/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     #log in page
     path('logout/', views.logoutuser, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    
     #food
     path('fooditems/', views.fooditems, name='food-items'),
     path('food/add/', views.addfood, name='add-food'),
